1b9fa9885d126c98a9544dc786a58ad6455f88ef
cebindani <danielecebin@hotmail.com>
2017-05-02 17:56:29 -0300

    Adjust Movie class to set image URL from path

--- app/src/main/java/com/dmaila/popularmoviesapp/Movie.java
+++ app/src/main/java/com/dmaila/popularmoviesapp/Movie.java
@@ -6,17 +6,23 @@ import android.net.Uri;
 public class Movie {
 
 
-    private Uri posterPath;
-    private Uri backdropPath;
+    private String posterPath;
+    private String posterUrl;
+    private String backdropPath;
+    private String backdropUrl;
     private long movieId;
     private String originalTitle;
     private String releaseDate;
     private String overview; //synopsis
     private double voteAverage; //user rating
+    public static final String BASE_URL = "image.tmdb.org/t/p";
+    public static final String IMG_SIZE = "w342";
 
-    public Movie(Uri posterPath, Uri backdropPath, long movieId, String originalTitle, String releaseDate, String overview, double voteAverage) {
+    public Movie(String posterPath, String backdropPath, long movieId, String originalTitle, String releaseDate, String overview, double voteAverage) {
         this.posterPath = posterPath;
+        this.posterUrl = generateImageURL(posterPath);
         this.backdropPath = backdropPath;
+        this.backdropUrl = generateImageURL(backdropPath);
         this.movieId = movieId;
         this.originalTitle = originalTitle;
         this.releaseDate = releaseDate;
@@ -27,19 +33,37 @@ public class Movie {
     public Movie() {
     }
 
-    public Uri getPosterPath() {
+    private String getPosterPath() {
         return posterPath;
     }
 
-    public void setPosterPath(Uri posterPath) {
+    public void setPosterPath(String posterPath) {
         this.posterPath = posterPath;
+        setPosterUrl(generateImageURL(posterPath));
     }
 
-    public Uri getBackdropPath() {
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    private void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getBackdropUrl() {
+        return backdropUrl;
+    }
+
+    private void setBackdropUrl(String backdropUrl) {
+        this.backdropUrl = backdropUrl;
+        setBackdropUrl(generateImageURL(backdropPath));
+    }
+
+    private String getBackdropPath() {
         return backdropPath;
     }
 
-    public void setBackdropPath(Uri backdropPath) {
+    public void setBackdropPath(String backdropPath) {
         this.backdropPath = backdropPath;
     }
 
@@ -95,4 +119,18 @@ public class Movie {
                 ", voteAverage=" + voteAverage +
                 '}';
     }
+
+    private String generateImageURL(String imagePath) {
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http")
+                .encodedAuthority(BASE_URL)
+                .path(IMG_SIZE)
+                .appendEncodedPath(imagePath)
+                .build();
+        String imageUrl = builder.toString();
+        return imageUrl;
+
+    }
+
 }
