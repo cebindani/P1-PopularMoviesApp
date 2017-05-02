50d22fe6c6e0bed1289af55b565a5da8b810da24
cebindani <danielecebin@hotmail.com>
2017-05-02 17:59:45 -0300

    Define custom adapter and populate RecyclerView

--- app/src/main/java/com/dmaila/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/dmaila/popularmoviesapp/MainActivity.java
@@ -4,15 +4,16 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ImageView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.dmaila.popularmoviesapp.BuildConfig.MOVIE_DB_API_KEY;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AsyncResponse {
 
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -22,27 +23,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String baseUrl = "http://image.tmdb.org/t/p/";
-        final String imageSize = "w185";
-        final String posterPath = "/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
-
-        String url = baseUrl + imageSize + posterPath;
-        Log.d(LOG_TAG, "url = " + url);
-
-        ImageView imageView = (ImageView) findViewById(R.id.banner_imageView);
-
-        Picasso.with(this)
-                .setIndicatorsEnabled(true);
-        Picasso.with(this)
-                .load(url)
-                .into(imageView);
-
-
         String apiUrlString = "http://api.themoviedb.org/3/movie/popular?api_key=" + MOVIE_DB_API_KEY;
 
         NetworkInfo networkInfo = getNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            new FetchMoviesData().execute(apiUrlString);
+            new FetchMoviesData(this).execute(apiUrlString);
         } else {
             Toast.makeText(MainActivity.this, R.string.message_offline, Toast.LENGTH_SHORT).show();
         }
@@ -53,5 +38,22 @@ public class MainActivity extends AppCompatActivity {
     private NetworkInfo getNetworkInfo() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         return connectivityManager.getActiveNetworkInfo();
+    }
+
+    @Override
+    public void onSuccess(List<Movie> moviesList) {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, 3);
+
+        RecyclerView myRecyclerView = (RecyclerView) findViewById(R.id.movies_recyclerview);
+        myRecyclerView.setLayoutManager(gridLayoutManager);
+
+        CustomAdapter customAdapter = new CustomAdapter(new ArrayList<Movie>(moviesList), MainActivity.this);
+        myRecyclerView.setAdapter(customAdapter);
+
+    }
+
+    @Override
+    public void onFailure() {
+
     }
 }
